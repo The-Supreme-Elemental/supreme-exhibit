b78eccdbbeb1df463304c7dd000fafa6a004d7cc
Christopher Hylton <chrisjh5@byui.edu>
2025-04-22 00:47:39 -0600

    Cleaned up submodule remains

---
+++ Flask Weather App/README.md
@@ -0,0 +1,4 @@
+## This is a Weather App in Flask via Python. 
+
+This app will allow a user to input a city and receive the weather from that city for that day. I aim to have this project reference a live database, probably from a weather website, and display it to 
+the user. At this current moment, the program is only able to display the weather for the current moment, but I plan on making it so that multiple forecasts can be displayed. 
---
+++ Flask Weather App/weather.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify, json
+from flask import render_template
+import requests
+
+app = Flask(__name__)
+
+API_KEY = "4cbbff0a538e1d58b8874be5b876ae76"  
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+@app.route('/')
+def home():
+    return render_template('weather.html')
+
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    city = request.args.get('city')
+    if not city:
+        return jsonify({"error": "Please provide a city name"}), 400
+
+    params = {"q": city,
+              "appid": API_KEY, 
+              "units": "metric"}
+    response = requests.get(BASE_URL, params=params)
+
+    print("API Response Code", response.status_code)  # Debugging line to check the response code
+    print("API Response", response.text)  # Debugging line to check the response content
+
+    if response.status_code == 200:
+        try: 
+            data = response.json()
+            weather_info = {
+                "city": data["name"],
+                "temperature": data["main"]["temp"],
+                "description": data["weather"][0]["description"]
+        }
+            print(json.dumps(data, indent=4))
+
+            print("Weather Info", weather_info)
+            print("Weather info passed to template:", weather_info)  #
+            # Return JSON response (for API use case)
+            # Instead of returning JSON, you can pass the data to the template
+            return render_template('weather_result.html', weather_info=weather_info)  # Render the HTML template with weather data
+        except KeyError as e:
+            print(f"KeyError: Missing key {e}")
+    else:
+        return jsonify({"error": "City not found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
